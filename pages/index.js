b97d15a7e6a381a9420ed0fcb1f63f038d802c8a
manuhon99 <manuhon99@gmail.com>
2021-02-25 00:33:40 -0300

    sending files

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,46 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
+
+export default function Home({ tvShowsList }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>RPC</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <Link href="/states">
+        <a>Check show</a>
+      </Link>
+      <h1>EPG</h1>
+      
+      {tvShowsList.map((show) => (
+        <div key={show.midia_id}>
+          <p>{show.title}</p>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      ))}
     </div>
-  )
+  );
+}
+
+export async function getStaticProps( ) {
+  var date = new Date(); 
+  var today = new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString();
+  var yesterday = new Date(date.getTime() - (date.getTimezoneOffset() * 60000)-86400000).toISOString();
+  var tomorrow = new Date(date.getTime() - (date.getTimezoneOffset() * 60000)+86400000).toISOString();
+  var query = [today, yesterday, tomorrow]
+  const query_id = query.map((query_id)=>{
+  console.log(query_id)
+  })
+  
+  const res = await fetch(`https://epg-api.video.globo.com/programmes/1337/?day=${query_id}`);
+  const guide = await res.json();
+  //console.log(guide.programme.entries.slice(0, 10))
+
+  return {
+    props: {
+      tvShowsList: guide.programme.entries
+    }
+  };
 }
